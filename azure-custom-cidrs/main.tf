0b5b7d2e9e30884b0e97944e658dc6c420a7918b
Creighton Leif <creighton.leif@gmail.com>
2025-04-11 13:15:01 -0500

    Update main.tf

--- azure-custom-cidrs/main.tf
+++ azure-custom-cidrs/main.tf
@@ -1,21 +1,8 @@
-module "tags_standards_prod" {
-  source = "git::https://conagra@dev.azure.com/conagra/cloud-services/_git/shared-infra-modules//modules/tagging?ref=v1.0.1-tagging"
-
-  ApplicationID        = "16396"
-  Environment          = "Production"
-  MetalTier            = "Gold"
-  DisasterRecoveryTier = "5"
-  Portfolio            = "Cloud Infrastructure & Operations"
-  Product              = "Data Center"
-  Repo                 = "azure-terraform-conagra-custom-cidr"
-  ADOProject           = "data-center-and-network-services"
-}
-
 resource "azurerm_resource_group" "rg_for_cidr_prefix" {
   name     = "rg-global-custom-cidr"
   location = "North Central US"
 
-  tags = module.tags_standards_prod.tags
+  tags = {}
 }
 
 resource "azurerm_custom_ip_prefix" "cdir_global" {
@@ -31,7 +18,7 @@ resource "azurerm_custom_ip_prefix" "cdir_global" {
   wan_validation_signed_message = "<signed_string_here>" #NEEDED FROM NETWORK
 
 
-  tags = module.tags_standards_prod.tags
+  tags = {}
 
 }
 
@@ -56,7 +43,7 @@ resource "azurerm_public_ip_prefix" "ncus_public_prefix" {
 
   prefix_length = 26
 
-  tags = module.tags_standards_prod.tags
+  tags = {}
 
 }
 
@@ -70,7 +57,7 @@ resource "azurerm_custom_ip_prefix" "cus_regional" {
   cidr                          = "1.2.3.4/26"
   zones                         = ["1", "2", "3"]
 
-  tags = module.tags_standards_prod.tags
+  tags = {}
 
 }
 
@@ -82,83 +69,6 @@ resource "azurerm_public_ip_prefix" "cus_public_prefix" {
 
   prefix_length = 26
 
-  tags = module.tags_standards_prod.tags
-
-}
-
-resource "azurerm_custom_ip_prefix" "eus2_regional" {
-  name                          = "cidr-eus2-region01"
-  location                      = "East US 2"
-  resource_group_name           = azurerm_resource_group.rg_for_cidr_prefix.name
-  parent_custom_ip_prefix_id    = azurerm_custom_ip_prefix.cdir_global.id
-  commissioning_enabled         = true
-  internet_advertising_disabled = true
-  cidr                          = "1.2.3.4/26"
-  zones                         = ["1", "2", "3"]
-
-  tags = module.tags_standards_prod.tags
-
-}
-
-resource "azurerm_public_ip_prefix" "eus2_public_prefix" {
-  name                = "public-eus2-prefix"
-  location            = azurerm_custom_ip_prefix.eus2_regional.location
-  resource_group_name = azurerm_resource_group.rg_for_cidr_prefix.name
-  zones               = ["1", "2", "3"]
-
-  prefix_length = 26
-
-  tags = module.tags_standards_prod.tags
-
-}
-
-resource "azurerm_custom_ip_prefix" "scus_regional" {
-  name                          = "cidr-scus-region01"
-  location                      = "South Central US"
-  resource_group_name           = azurerm_resource_group.rg_for_cidr_prefix.name
-  parent_custom_ip_prefix_id    = azurerm_custom_ip_prefix.cdir_global.id
-  commissioning_enabled         = true
-  internet_advertising_disabled = true
-  cidr                          = "204.76.111.128/26"
-  zones                         = ["1", "2", "3"]
-
-  tags = module.tags_standards_prod.tags
-
-}
-
-resource "azurerm_public_ip_prefix" "scus_public_prefix" {
-  name                = "public-scus-prefix"
-  location            = azurerm_custom_ip_prefix.scus_regional.location
-  resource_group_name = azurerm_resource_group.rg_for_cidr_prefix.name
-  zones               = ["1", "2", "3"]
-  
-  prefix_length       = 26
-
-  tags = module.tags_standards_prod.tags
-
-}
-resource "azurerm_custom_ip_prefix" "cin_regional" {
-  name                          = "cidr-cin-region01"
-  location                      = "Central India"
-  resource_group_name           = azurerm_resource_group.rg_for_cidr_prefix.name
-  parent_custom_ip_prefix_id    = azurerm_custom_ip_prefix.cdir_global.id
-  commissioning_enabled         = true
-  internet_advertising_disabled = true
-  cidr                          = "1.2.3.4/26"
-  zones                         = ["1", "2", "3"]
-
-  tags = module.tags_standards_prod.tags
-
-}
-
-resource "azurerm_public_ip_prefix" "cin_public_prefix" {
-  name                = "public-cin-prefix"
-  location            = azurerm_custom_ip_prefix.cin_regional.location
-  resource_group_name = azurerm_resource_group.rg_for_cidr_prefix.name
-  zones               = ["1", "2", "3"]
-
-  prefix_length = 26
-
-  tags = module.tags_standards_prod.tags
+  tags = {}
 
 }
